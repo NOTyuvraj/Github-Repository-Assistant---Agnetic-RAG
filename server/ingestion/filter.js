f5e410366dd7d5c6bbfb8012521849281a2bffe9
NOTyuvraj <cyuvraj980@gmail.com>
2026-06-23 13:37:05 +0530

    Finished filter.js (Filtering for required files )

--- server/ingestion/filter.js
+++ server/ingestion/filter.js
@@ -7,25 +7,24 @@ const unwantedDir = ["node_modules", ".git"];
 
 const unwantedFile = ["package-lock.json", "yarn.lock"];
 
-const fileList = [];
+
 
 export const filter = async (repoDir) => {
+  const fileList = [];
   try {
-    
     const files = await readdir(repoDir, { withFileTypes: true });
-
     for (const file of files) {
       if (file.isDirectory()) {
         if (unwantedDir.includes(file.name)) continue;
         const fullpath = path.join(repoDir, file.name);
-        await filter(fullpath);
-        console.log(fullpath);
+        const nestedFiles = await filter(fullpath);
+        fileList.push(...nestedFiles);
       } else {
         if (unwantedFile.includes(file.name)) continue;
         const extensionName = path.extname(file.name);
         if (unwantedExt.includes(extensionName)) continue;
-        const fullpath = path.join(repoDir , file.name);
-        fileList.push(fullpath)
+        const fullpath = path.join(repoDir, file.name);
+        fileList.push(fullpath);
       }
     }
     return fileList;
